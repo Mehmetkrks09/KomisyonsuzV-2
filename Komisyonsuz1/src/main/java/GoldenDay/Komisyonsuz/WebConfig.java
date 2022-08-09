1de97c41a13354f38b0ee7213ef230cbac018a56
Mehmetkrks09 <mehmetkarakas415@gmail.com>
2022-08-09 15:04:49 +0300

    asda

--- Komisyonsuz1/src/main/java/GoldenDay/Komisyonsuz/WebConfig.java
+++ Komisyonsuz1/src/main/java/GoldenDay/Komisyonsuz/WebConfig.java
@@ -1,6 +1,5 @@
 package GoldenDay.Komisyonsuz;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
